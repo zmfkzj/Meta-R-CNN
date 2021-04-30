7e7f59368f44ce12d984533cc38cb4184ad640e7
zmfkzj <qlwlal@naver.com>
2021-04-30 08:30:07 +0000

    init

---
+++ lib/datasets/__init__.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = 1000000000
